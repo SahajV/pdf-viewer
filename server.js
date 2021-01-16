e6ff37b46d8dda7c44cf227ac93a6a587c297183
SahajV <vedereysahaj1@gmail.com>
2021-01-16 17:32:24 -0500

    heroku try 4

--- server.js
+++ server.js
@@ -37,8 +37,7 @@ app.post('/upload',function(req, res) {
 
 });
 
-app.listen(8000, function() {
-
-    console.log('App running on port 8000');
-
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`)
 });
